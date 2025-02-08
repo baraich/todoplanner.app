86bc98fcba172b0bf144a83345e6606690645b2d
Baraich <gurvirsingh.baraich@gmail.com>
2025-02-08 23:59:47 +0530

    feat(www): completed the landing page for the website

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from "astro/config";
 import aws from "astro-sst";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: aws(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
